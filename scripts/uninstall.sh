2fd59aa6de00b5b77fcc8791fc9007cf18997493
Anonymoussaurus <50231698+AnonymousWP@users.noreply.github.com>
2024-06-08 13:52:48 +0200

    build(uninstall): replace echo by log

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -4,100 +4,100 @@ tput setaf 2;
 cat ../web/art/reNgine.txt
 
 tput setaf 3;
-echo ""
-echo "Uninstalling reNGine"
+log ""
+log "Uninstalling reNGine"
 
 if [ "$EUID" -ne 0 ]
   then
-  echo ""
-  echo "Error uninstalling reNGine, Please run this script as root!"
-  echo "Example: sudo ./uninstall.sh"
+  log ""
+  log "Error uninstalling reNGine, Please run this script as root!"
+  log "Example: sudo ./uninstall.sh"
   exit
 fi
 
 tput setaf 1;
-echo ""
+log ""
 read -p "This action will stop and remove all containers, volumes and networks of reNGine. Do you want to continue? [y/n] " -n 1 -r
-echo ""
+log ""
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo ""
+  log ""
   tput setaf 3;
-  echo "Stopping reNGine"
+  log "Stopping reNGine"
   tput setaf 4;
   docker stop rengine-web-1 rengine-db-1 rengine-celery-1 rengine-celery-beat-1 rengine-redis-1 rengine-proxy-1
   tput setaf 3;
-  echo "Stopped reNGine"
-  echo ""
+  log "Stopped reNGine"
+  log ""
 
-  echo "Removing all containers related to reNGine"
+  log "Removing all containers related to reNGine"
   tput setaf 4;
   docker rm rengine-web-1 rengine-db-1 rengine-celery-1 rengine-celery-beat-1 rengine-redis-1 rengine-proxy-1
   tput setaf 3;
-  echo "Removed all containers related to reNGine"
-  echo ""
+  log "Removed all containers related to reNGine"
+  log ""
 
-  echo "Removing all volumes related to reNGine"
+  log "Removing all volumes related to reNGine"
   tput setaf 4;
   docker volume rm rengine_gf_patterns rengine_github_repos rengine_nuclei_templates rengine_postgres_data rengine_scan_results rengine_tool_config rengine_static_volume rengine_wordlist
   tput setaf 3;
-  echo "Removed all volumes related to reNGine"
-  echo ""
+  log "Removed all volumes related to reNGine"
+  log ""
 
-  echo "Removing all networks related to reNGine"
+  log "Removing all networks related to reNGine"
   tput setaf 4;
   docker network rm rengine_rengine_network rengine_default
   tput setaf 3;
-  echo "Removed all networks related to reNGine"
-  echo ""
+  log "Removed all networks related to reNGine"
+  log ""
 else
   tput setaf 2;
-  echo ""
-  echo "Exiting!"
+  log ""
+  log "Exiting!"
   exit 1
 fi
 
 tput setaf 1;
 read -p "Do you want to remove Docker images related to reNGine? [y/n] " -n 1 -r
-echo ""
+log ""
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo ""
+  log ""
   tput setaf 3;
-  echo "Removing all Docker images related to reNGine"
+  log "Removing all Docker images related to reNGine"
   tput setaf 4;
   docker image rm rengine-celery rengine-celery-beat rengine-certs docker.pkg.github.com/yogeshojha/rengine/rengine nginx:alpine redis:alpine postgres:12.3-alpine
   tput setaf 3;
-  echo "Removed all Docker images"
-  echo ""
+  log "Removed all Docker images"
+  log ""
 else
   tput setaf 2;
-  echo ""
-  echo "Skipping removal of Docker images"
+  log ""
+  log "Skipping removal of Docker images"
 fi
 
 tput setaf 1;
 read -p "Do you want to remove all Docker-related leftovers? [y/n] " -n 1 -r
-echo ""
+log ""
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo ""
+  log ""
   tput setaf 3;
-  echo "Removing all Docker-related leftovers"
+  log "Removing all Docker-related leftovers"
   tput setaf 4;
   docker system prune -a -f
   tput setaf 3;
-  echo "Removed all Docker-related leftovers"
-  echo ""
+  log "Removed all Docker-related leftovers"
+  log ""
 else
-  echo ""
-  echo "Skipping removal of Docker-related leftovers"
-  echo ""
+  log ""
+  log "Skipping removal of Docker-related leftovers"
+  log ""
 fi
 
 
 tput setaf 2;
-echo "Finished uninstalling."
+log "Finished uninstalling."
