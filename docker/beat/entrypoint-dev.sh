0b1f3d9641bc142f521dfc31beecc0ff784c0419
psyray <psyray@users.noreply.github.com>
2024-08-20 03:17:12 +0200

    fix(ui): restore static files path & remove beat entrypoint useless code

--- docker/beat/entrypoint-dev.sh
+++ docker/beat/entrypoint-dev.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 if [ "$CELERY_DEBUG" == "1" ]; then
-    # Django debug toolbar
-    pip install django-debug-toolbar==4.3.0
-    python3 manage.py collectstatic --noinput
+    export CELERY_LOGLEVEL='debug'
 fi
 
 # Check if remote debugging is enabled and set concurrency to 1 for easier debug
 if [ "$CELERY_REMOTE_DEBUG" == "1" ]; then
-    # Live debug
-    pip install debugpy
-
-    # To debug opened port with netstat
-    apt install net-tools -y
+    # Set celery concurrency to 1 because thread processes is hard to debug
+    export MIN_CONCURRENCY=1
+    export MAX_CONCURRENCY=1
 fi
 
 /entrypoint.sh
