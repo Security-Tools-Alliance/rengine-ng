8629510cbb63a4080b48be6a489c497bf7535d16
psyray <psyray@users.noreply.github.com>
2024-08-21 19:37:02 +0200

    docker(web): fix bad labels

--- docker/web/entrypoint-dev.sh
+++ docker/web/entrypoint-dev.sh
@@ -18,7 +18,7 @@ print_msg "Collect static files"
 poetry run -C $HOME/ python3 manage.py collectstatic --noinput
 
 # Run development server
-print_msg "Migrate database"
+print_msg "Launching Django development Web server"
 poetry run -C $HOME/ python3 manage.py runserver 0.0.0.0:8000
 
 exec "$@"
