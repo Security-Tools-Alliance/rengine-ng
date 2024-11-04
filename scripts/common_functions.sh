c839bd2f61a9bcc841c65f1b4d70cf98f7dcf891
Anonymoussaurus <50231698+AnonymousWP@users.noreply.github.com>
2024-11-04 17:53:18 +0100

    build(docker): refactor detection of OS and add support for RHEL distros (#211)
    * build(docker): refactor detection of OS
    
    This commit also adds support for RHEL distros
    
    * build(install): use functions for installing dependencies
    
    * feat(install): enhance installation script with Docker Compose version check and refactor package installation
    
    - Refactored the installation script to include a Docker Compose version check, ensuring compatibility with version 2.0.0 or higher.
    - Consolidated package installation functions into a generic install_package function to streamline the installation process for various packages like nano, curl, and make.
    - Improved error handling and logging for package installations and Docker setup.
    
    * feat(install): enhance Docker and Docker Compose checks in installation script
    
    - Added functions to check Docker and Docker Compose installations, including version checks and guidance for installation or upgrade if necessary.
    - Removed automatic installation of Docker and Docker Compose, replacing it with user guidance for manual installation.
    - Updated the installation script to ensure Docker is installed and running before proceeding.
    
    * fix: improve Docker installation checks and update Makefile
    
    - Enhanced the Docker installation script to repeatedly prompt the user until a valid choice is made.
    - Updated the Makefile to include checks for Docker Compose installation and user permissions, ensuring the user is in the docker group or running as root.
    
    * build(install): move pre-req check after env check
    
    * build(install): specify color meaning and change some
    
    * build(install): provide more specific error information
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * build(make): use `getent`
    
    ---------
    
    Co-authored-by: psyray <psyray@users.noreply.github.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -2,13 +2,13 @@
 
 # Define color codes.
 COLOR_BLACK=0
-COLOR_RED=1
-COLOR_GREEN=2
-COLOR_YELLOW=3
+COLOR_RED=1 # For errors and important messages
+COLOR_GREEN=2 # For succesful output/messages
+COLOR_YELLOW=3 # For questions and choices
 COLOR_BLUE=4
 COLOR_MAGENTA=5
-COLOR_CYAN=6
-COLOR_WHITE=7
+COLOR_CYAN=6 # For actions that are being executed
+COLOR_WHITE=7 # Default, we don't really use this explicitly
 COLOR_DEFAULT=$COLOR_WHITE # Use white as default for clarity
 
 # Log messages in different colors
