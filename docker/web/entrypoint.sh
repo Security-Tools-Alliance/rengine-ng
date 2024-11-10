b3d5ce52a081e2135852ca4711620f5f90f9026f
psyray <psyray@users.noreply.github.com>
2024-11-10 17:47:04 +0100

    feat(docker): enhance gunicorn server configuration
    - Enhanced the Gunicorn server configuration in the Docker entrypoint script by adding timeout and keep-alive settings to improve server performance and reliability.

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -4,6 +4,11 @@
 poetry run -C $HOME/ python3 manage.py collectstatic --noinput
 
 # Run production server
-poetry run -C $HOME/ gunicorn reNgine.wsgi:application -w 8 --bind 0.0.0.0:8000 --limit-request-line 0
+poetry run -C $HOME/ gunicorn reNgine.wsgi:application \
+    --workers 8 \
+    --bind 0.0.0.0:8000 \
+    --limit-request-line 0 \
+    --timeout 120 \
+    --keep-alive 120
 
 exec "$@"
