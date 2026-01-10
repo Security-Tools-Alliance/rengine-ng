b67b3ea56eb3eabb5cfadbdc770c9e323ee2c513
0b3ud <0b3ud@proton.me>
2026-01-10 19:37:11 +0100

    chore: add OAuth token settings

--- web/reNgine/roles.py
+++ web/reNgine/roles.py
@@ -38,3 +38,7 @@ class Auditor(AbstractUserRole):
         PERM_INITATE_SCANS_SUBSCANS: False,
         PERM_MODIFY_TARGETS: False,
     }
+
+# OAuth user defaults
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+SOCIALACCOUNT_STORE_TOKENS = False  # Don't store OAuth tokens (security)
